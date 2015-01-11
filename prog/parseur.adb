86d3b937f6d8dc851e8b13cc11ab2bcd89d000c7
braavos <braavos@debianFlat>
2015-01-11 14:16:13 +0100

    Ajout fichier "simple.kn" pour test (donnée en exemple dans le sujet) Correction bug : les lignes étaient tracées 2 fois

--- prog/parseur.adb
+++ prog/parseur.adb
@@ -10,19 +10,18 @@ package body Parseur is
 	begin
 		Open (File => file, Mode => In_File, Name => filename);
 		Get(file, sommets);
-		--Put("Il y a ") ; Put(sommets, Width => 0) ; Put(" Sommets.");
+	--	Put("Il y a ") ; Put(sommets, Width => 0) ; Put(" Sommets.");
 	end;
 
 	procedure Lecture (filename : in String; sommets : in Natural; Figure : in out Graphe) is
-		nbLiaisons : Integer ;
 	begin
 		for i in 1..sommets loop
 			Get(file, Figure(i).all.Coord.all.X);
 			Get(file, Figure(i).all.Coord.all.Y);
-			Get(file, nbLiaisons);
-			Figure(i).all.Connexions := nbLiaisons;
-			Figure(i).all.Liens := new Liaisons (1 .. nbLiaisons);
-			for j in 1..nbLiaisons loop
+			Get(file, Figure(i).all.connexions);
+
+			Figure(i).all.Liens := new Liaisons (1 .. Figure(i).all.connexions);
+			for j in 1..Figure(i).all.connexions loop
 				Get(file, Figure(i).all.Liens.all(j));
 			end loop;
 		end loop;
--- prog/svg.adb
+++ prog/svg.adb
@@ -3,51 +3,57 @@ use Ada.Text_IO, Ada.Float_Text_IO, Ada.Integer_Text_IO;
 
 package body Svg is
 
-	Display_Width, Display_Height : Float;
+   Display_Width, Display_Height : Float;
 
-	procedure Svg_Line(P1, P2 : Point ; C : Color) is
-	begin
-		Put("<line x1=""");
-		Put(P1.X);
-		Put(""" y1=""");
-		Put(P1.Y);
-		Put(""" x2=""");
-		Put(P2.X);
-		Put(""" y2=""");
-		Put(P2.Y);
-		Put(""" style=""stroke:");
-		case C is
-			when Red => Put("rgb(255,0,0)");
-			when Green => Put("rgb(0,255,0)");
-			when Blue => Put("rgb(0,0,255)");
-			when Black => Put("rgb(0,0,0)");
-		end case;
-		Put_Line(";stroke-width:0.2""/>");
-	end Svg_Line;
+   procedure Svg_Line(P1, P2 : Point ; C : Color) is
+   begin
+      Put("<line x1=""");
+      Put(P1.X);
+      Put(""" y1=""");
+      Put(P1.Y);
+      Put(""" x2=""");
+      Put(P2.X);
+      Put(""" y2=""");
+      Put(P2.Y);
+      Put(""" style=""stroke:");
+      case C is
+	 when Red => Put("rgb(255,0,0)");
+	 when Green => Put("rgb(0,255,0)");
+	 when Blue => Put("rgb(0,0,255)");
+	 when Black => Put("rgb(0,0,0)");
+      end case;
+      Put_Line(";stroke-width:0.2""/>");
+   end Svg_Line;
 
-	procedure Svg_Header(Width, Height : Integer) is
-	begin
-		Put("<svg width=""");
-		Put(Width);
-		Put(""" height=""");
-		Put(Height);
-		Put_Line(""">");
-		Display_Width := Float(Width);
-		Display_Height := Float(Height);
-	end Svg_Header;
+   procedure Svg_Header(Width, Height : Integer) is
+   begin
+      Put("<svg width=""");
+      Put(Width);
+      Put(""" height=""");
+      Put(Height);
+      Put_Line(""">");
+      Display_Width := Float(Width);
+      Display_Height := Float(Height);
+   end Svg_Header;
 
-	procedure TracerAretes(Figure : in Graphe; Sommets : in Natural) is
-	begin
-		for i in 1..Sommets loop
-			for j in 1..Figure(i).all.Connexions loop
-				Svg_Line(Figure(i).all.Coord.all, Figure(Figure(i).all.Liens.all(j)).all.Coord.all, Black);
-			end loop;
-		end loop;
-	end TracerAretes;
+   procedure TracerAretes(Figure : in Graphe; nbSommets : in Natural) is
+   begin
+      for i in 1..nbSommets loop
+	 for j in 1..Figure(i).all.Connexions loop
 
-	procedure Svg_Footer is
-	begin
-		Put_Line("</svg>");
-	end Svg_Footer;
+	   if Figure(i).all.Liens.all(j) > i then -- if the line hasn't been drawn yet
+	       Svg_Line(Figure(i).all.Coord.all, 
+		     Figure(Figure(i).all.Liens.all(j)).all.Coord.all, 
+		     Black);
+	   end if;
+
+	 end loop;
+      end loop;
+   end TracerAretes;
+
+   procedure Svg_Footer is
+   begin
+      Put_Line("</svg>");
+   end Svg_Footer;
 
 end Svg;
